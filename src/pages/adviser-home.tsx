d2bd1e94bcd5304349512c36a2cbb9ff3b5f1f66
Jameel <jambamuk@gmail.com>
2022-09-13 08:52:01 +0000

    working pokemon add will conflict with home work

--- src/pages/adviser-home.tsx
+++ src/pages/adviser-home.tsx
@@ -1,18 +1,47 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import { Pokemon, PokemonClient } from "pokenode-ts"
+import { useEffect, useState } from "react"
+import Image from "next/image"
 
 export default function AdviserHome() {
+  const [data, setData] = useState<Pokemon[]>([])
+  const [isLoading, setLoading] = useState(false)
+
+  useEffect(() => {
+    addPokemon()
+  }, [])
+
+  function addPokemon() {
+
+    const api = new PokemonClient();
+    setLoading(true)
+    api
+      .getPokemonById(Math.round(Math.random() * 10 + 1))
+      .then((p) => {
+        console.log(p)
+        if (p) {
+          setData([...data, p])
+          setLoading(false)
+        }
+      })
+  }
+  if (isLoading) return <p>Loading...</p>
+  if (!data) return <p>No profile data</p>
+
   return (
-    <>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <h1 className="bg-blue">First Post</h1>
-      <h2>
-        <Link href="/">
-          <a className="">Back to home</a>
-        </Link>
-      </h2>
-    </>
-  )
-}
+    <div>
+      <button onClick={addPokemon}>Add</button>
+      {data &&
+        data.map((p) => (
+          <div>
+            <Image
+              src={p.sprites.front_default}
+              alt={p.name}
+              width={100}
+              height={100}
+            />
+          </div>
+        ))
+      }
+    </div>
+  );
+};
